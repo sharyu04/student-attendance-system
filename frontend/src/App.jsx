b3c05d59b4464bc4b9960f188f533eb7607abd20
Sharyu <sharayumarwadi11@gmail.com>
2023-01-06 19:48:41 +0530

    react website initiated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+      <div className="register-student">
+        
+      </div>
+      <div className="student-info"></div>
+    </div>
+  );
+}
+
+export default App;
